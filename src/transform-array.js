8c96f5298cc4530b196fc598769048a57e7b221b
Pavel Urbanovich <yrbik@mail.ru>
2022-03-01 23:05:39 +0300

    feat: add solution transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  const discardNext = '--discard-next';
+  const discardPrev = '--discard-prev';
+  const doubleNext = '--double-next';
+  const doublePrev = '--double-prev';
+  
+  if (arr instanceof Array === false) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  
+  const result = [];
+  const array = [...arr]
+
+  for(let i = 0; i < array.length; i++) {
+
+    if (array[i] === discardPrev && i === 0) {
+      continue
+    }
+
+    if (array[i] === doublePrev && i === 0) {
+      continue;
+      
+    }
+    if ((arr[i] === doubleNext && i === arr.length - 1) || (arr[i] === discardNext && i === arr.length - 1)) {
+     return result;
+    }
+
+    if (array[i] === doubleNext) {
+      array[i] = array[i + 1]
+    }
+
+    if (array[i] === doublePrev) {
+      array[i] = array[i - 1]
+    }
+
+    if (array[i] === discardPrev && i !== 0) {
+      result.pop();
+      continue;
+    }
+
+    if (array[i] === discardNext) {
+      i = i + 1;
+     continue;
+    }
+
+    if(array[i] === discardNext && array[i + 2] === discardPrev) {
+      array[i] = array[i + 2]
+    }
+
+
+   result.push(array[i])
+    }
+    
+    return result;
 }
 
 module.exports = {
